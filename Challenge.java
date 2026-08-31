cb0bd4d58f440cc99ea7d3eb9970a1a9d8dcd17e
Munkh26 <munkhbatmunkhsoyombo@gmail.com>
2026-08-31 17:14:49 +0000

    finish

--- Challenge.java
+++ Challenge.java
@@ -1,3 +1,7 @@
+//Name: Munkhsoyombo Munkhbat
+//Date: 08/31/2026
+//Description: It decipher the text in Python Challenge Level 1
+
 public class Challenge {
     public static void main(String[] args) {
         String message = "g fmnc wms bgblr rpylqjyrc gr zw fylb. rfyrq ufyr amknsrcpq ypc dmp. bmgle gr gl zw fylb gq glcddgagclr ylb rfyr'q ufw rfgq rcvr gq qm jmle. sqgle qrpgle.kyicrpylq() gq pcamkkclbcb. lmu ynnjw ml rfc spj.";
@@ -11,7 +15,7 @@ public class Challenge {
                 else if (letter == 121) {
                     letter = 97;
                 }
-                else if (letter < 47 || letter > 122) {
+                else if (letter < 48 || letter > 122) {
 
                 }
                 else {
